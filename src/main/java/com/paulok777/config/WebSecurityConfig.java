38ccb9ec3909db01603715457b4fb87e7c49b0e1
PauLOk777 <trocjuk2000@gmail.com>
2020-10-04 15:36:19 +0300

    Added authentification handler for success login.

--- src/main/java/com/paulok777/config/WebSecurityConfig.java
+++ src/main/java/com/paulok777/config/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package com.paulok777.config;
 import com.paulok777.entity.Role;
 import com.paulok777.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 
 @Configuration
@@ -21,27 +23,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/commodity_expert/products", "/commodity_expert/products/{id}")
-                .hasAuthority(Role.COMMODITY_EXPERT.getAuthority())
+                    .hasAuthority(Role.COMMODITY_EXPERT.getAuthority())
                 .antMatchers(
                         "/senior_cashier/orders/cancel/{orderId}/{productId}",
                         "/senior_cashier/orders/cancel/{id}", "/senior_cashier/reports/x",
                         "/senior_cashier/reports/z", "/senior_cashier/orders", "/senior_cashier/orders/{id}",
                         "/senior_cashier/orders/{orderId}/{productId}", "/senior_cashier/orders/close/{id}")
-                .hasAuthority(Role.SENIOR_CASHIER.getAuthority())
+                    .hasAuthority(Role.SENIOR_CASHIER.getAuthority())
                 .antMatchers("/cashier/orders", "/cashier/orders/{id}",
                         "/cashier/orders/{orderId}/{productId}", "/cashier/orders/close/{id}")
-                .hasAnyAuthority(Role.CASHIER.getAuthority())
+                    .hasAnyAuthority(Role.CASHIER.getAuthority())
                 .antMatchers("/logout")
-                .hasAnyAuthority(Role.CASHIER.getAuthority(), Role.SENIOR_CASHIER.getAuthority(), Role.COMMODITY_EXPERT.getAuthority())
+                    .hasAnyAuthority(Role.CASHIER.getAuthority(), Role.SENIOR_CASHIER.getAuthority(), Role.COMMODITY_EXPERT.getAuthority())
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/", "/login", "/registration").anonymous()
                 .and()
-                .formLogin()
-                .loginPage("/login");
+                    .formLogin()
+                    .loginPage("/login")
+                    .successHandler(myAuthenticationSuccessHandler());
     }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return new UrlAuthenticationSuccessHandler();
     }
 }
